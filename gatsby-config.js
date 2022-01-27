fd61e5d6f19090252c3f14e081777cd69f32fc87
ishaan1013 <ishaan1013@gmail.com>
2022-01-27 12:00:01 -0500

    font + colours

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,9 @@ module.exports = {
         // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        fonts: [
+          `Roboto Condensed\:300,400,700`
+        ]
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/globals/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+    red1: '#E63A46',
+    blue1: '#457BA0',
+    blue2: '#1D3557',
+    background: '#0D131B',
+    white: '#fff',
+}

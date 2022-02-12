b3732b3075d170bdf4899692963bd402b8b6550b
ishaan1013 <ishaan1013@gmail.com>
2022-02-11 22:36:29 -0500

    fix: light/dark theme on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,29 @@ import Layout from "../components/PageLayout/layout"
 import Seo from "../components/SEO/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <Layout className="index-layout">
     <Seo title="Home" />
-    <div className="hero-section">
-      <div className="left">
-        <h1 className="large-header">
-          BRINGING <span className="red-text large-header">E-SPORTS</span> TO HIGH SCHOOLS IN ONTARIO
-        </h1>
-        <h3 className="subtitle">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
-        </h3>
-        <div>
-          <div className="btn-fill">Get Started</div>
+    <div className="dark-bg">
+      <div className="hero-section">
+        <div className="left">
+          <h1 className="large-header">
+            BRINGING <span className="red-text large-header">ESPORTS</span> TO HIGH SCHOOLS IN ONTARIO
+          </h1>
+          <h3 className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore...
+          </h3>
+          <div>
+            <div className="btn-fill">Get Started</div>
+          </div>
+        </div>
+        <div className="right">
+          <img src={require('../images/TestDisplay.png').default} alt="test display img"/>
         </div>
       </div>
-      <div className="right">
-        <img src={require('../images/TestDisplay.png').default} alt="test display img"/>
+      <div className="events-section">
+        <div className="medium-header">
+          Upcoming Events
+        </div>
       </div>
     </div>
   </Layout>

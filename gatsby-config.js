c3c3a2561a67deb96eb21abc1bafa40c0c8f0452
Kenny Zhao <kennyzhao2004@gmail.com>
2022-02-14 10:31:45 -0500

    Edit site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `OHSEA`,
+    description: `Welcome to the official website of the Ontario High School Esports Association. All important updates and information will be posted here, along with general information, including goals and about us.`,
+    author: `@OHSEA`,
+    siteUrl: `https://ohsea.gg/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,15 +21,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ontario High School Esports Association`,
+        short_name: `OHSEA`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
         fonts: [
           `Roboto Condensed\:300,400,700`
         ]

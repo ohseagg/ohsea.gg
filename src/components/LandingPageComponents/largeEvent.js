ff7ed19c135e3afd904bdaf496d5861ce3685a79
ishaan1013 <ishaan1013@gmail.com>
2022-02-23 22:22:10 -0500

    feat: event box content start

--- src/components/LandingPageComponents/largeEvent.js
+++ src/components/LandingPageComponents/largeEvent.js
@@ -3,11 +3,13 @@ import { Link } from "gatsby"
 import "../../styles/style.scss"
 
 const LargeEvent = () => {
-        return (
-            <div className="l-event-container">
-                
-            </div>
-        )
-  }
+  return (
+      <div className="l-event-container">
+          <div className="event-header">EVENT NAME</div>
+
+          <div className="btn-fill">See Details</div>
+      </div>
+  )
+}
 
 export default LargeEvent;

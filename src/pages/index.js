8f7cf272641e3d34c1fafd4ae297f54cd1c6860d
ishaan1013 <ishaan1013@gmail.com>
2022-02-28 22:23:17 -0500

    fix: landing page about section started

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/SEO/seo"
 import HeroSection from "../components/LandingPageComponents/heroSection"
 import LargeEvent from "../components/LandingPageComponents/largeEvent"
 //import SmallEvent from "../components/LandingPageComponents/smallEvent"
-import WelcomeSnippet from "../components/LandingPageComponents/welcomeSnippet/welcomeSnippet";
+// import AboutSnippet from "../components/LandingPageComponents/aboutSnippet/aboutSnippet";
 
 import data from "../data/landingEventData"
 import  "../styles/style.scss"
@@ -39,27 +39,36 @@ const IndexPage = () => (
           See More <GoChevronRight className="chevron-link"/>
         </Link> */}
       </div>
+    </div>
 
-      <WelcomeSnippet/>
+    {/* <AboutSnippet/> */}
 
-      <div className="text-section">
-        <div className="section">
-          <div className="header">Join Our Community</div>
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-          <div className="link-container">
-            <a href="https://discord.com/invite/zWc8fymdrG" target="_blank" className="red-text" >
-              Join Discord Server <GoChevronRight className="chevron-link"/>
-            </a>
-          </div>
+    <div className="text-section">
+      <div className="section">
+        <div className="header">Join Our Community</div>
+        <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className="link-container">
+          <a href="https://discord.com/invite/zWc8fymdrG" target="_blank" className="red-text" >
+            Join Discord Server <GoChevronRight className="chevron-link"/>
+          </a>
         </div>
-        <div className="section">
-          <div className="header">Bring Esports To Your School</div>
-          <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-          <div className="link-container">
-            <Link to="/" className="red-text" >
-              Contact Us <GoChevronRight className="chevron-link"/>
-            </Link>
-          </div>
+      </div>
+      <div className="section">
+        <div className="header">Bring Esports To Your School</div>
+        <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className="link-container">
+          <Link to="/" className="red-text" >
+            Contact Us <GoChevronRight className="chevron-link"/>
+          </Link>
+        </div>
+      </div>
+    </div>
+
+    <div className="dark-bg-container-2">
+      <div className="dark-bg">
+        <div className="about-snippet-container">
+          <div className="medium-header"><span className="red-text">Esports</span> In High School</div>
+          {/* todo add abbreviated version of kenny's about page component */}
         </div>
       </div>
     </div>

7e199c72a0c82ad87c81c556590053e6375234c0
ishaan1013 <ishaan1013@gmail.com>
2022-03-08 18:20:31 -0500

    fix: update header sizing & capitalization on landing page

--- src/components/LandingPageComponents/heroSection.js
+++ src/components/LandingPageComponents/heroSection.js
@@ -7,7 +7,7 @@ export default function HeroSection() {
         <div className="hero-section">
             <div className="left">
                 <h1 className="large-header">
-                    BRINGING <span className="red-text large-header">ESPORTS</span> TO HIGH SCHOOLS IN ONTARIO
+                    Bringing <span className="red-text large-header">ESPORTS</span> To High Schools In Ontario
                 </h1>
                 <h3 className="subtitle">
                     More than just a game, Esports opens pathways to STEM careers, builds soft skills, and allows students to connect with one another.

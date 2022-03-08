e115c5b063d98d802f0e88be8ff21f4a7a1a9eb1
ishaan1013 <ishaan1013@gmail.com>
2022-03-08 17:57:13 -0500

    feat: contact page left section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -25,74 +25,75 @@ function validateMessage(value) {
     return error;
 }
 
-class Contact extends React.Component {
-    render() {
-        const form = useRef()
+export default function Contact() {
+    const form = useRef()
 
-        return(
-            <Layout>
-                <Seo title="Contact" />
-                <div className="plain-dark-bg">
-                    <div className="container-contact">
-                        <div className="left">
-                            
-                        </div>
-                        <div className="right">
-                        <Formik
-                            initialValues={{
-                                email: '',
-                                msg: '',
-                            }}
-                            onSubmit={async (values) => {
-                                await new Promise((r) => setTimeout(r, 500));
-                                alert(JSON.stringify(values, null, 2));
-                            }}
-                            validateOnChange={false}
-                            validateOnBlur={false}
-                            >
-                            {({ errors, touched, isValidating }) => (
-                                <Form>
-                                    <div className="form-wrapper">
-                                        <div className="input-field">
-                                            <label htmlFor="email">Your email address</label>
-                                            <Field
-                                            id="email"
-                                            name="email"
-                                            placeholder="student@ohsea.gg"
-                                            as="input"
-                                            validate={validateEmail}
-                                            />
-                                            {errors.email && touched.email && 
-                                            <div className="error"><RiErrorWarningLine className="warning-icon"/> {errors.email}</div>
-                                            }
-                                        </div>
-                                        <div className="input-field">
-                                            <label htmlFor="msg">How can we help?</label>
-                                            <Field
-                                            id="msg"
-                                            name="msg"
-                                            placeholder="Your Message"
-                                            as="textarea"
-                                            validate={validateMessage}
-                                            />
-                                            {errors.msg && touched.msg && 
-                                            <div className="error"><RiErrorWarningLine className="warning-icon"/> {errors.msg}</div>
-                                            }
-                                        </div>
-                                        <button 
-                                        type="submit"
-                                        className="btn-fill"
-                                        >Send</button>
-                                    </div>
-                                </Form>
-                            )}
-                            </Formik>
+    return(
+        <Layout>
+            <Seo title="Contact" />
+            <div className="plain-dark-bg">
+                <div className="container-contact">
+                    <div className="left">
+                        <h1 className="large-header">
+                            Let's talk.
+                        </h1>
+                        <div className="subtitle">
+                            We'd love to hear from you, whether you're a student with some questions, an organization seeking a partnership, or anyone else!
                         </div>
                     </div>
+                    <div className="right">
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            msg: '',
+                        }}
+                        onSubmit={async (values) => {
+                            await new Promise((r) => setTimeout(r, 500));
+                            alert(JSON.stringify(values, null, 2));
+                        }}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        >
+                        {({ errors, touched, isValidating }) => (
+                            <Form>
+                                <div className="form-wrapper">
+                                    <div className="input-field">
+                                        <label htmlFor="email">Your email address</label>
+                                        <Field
+                                        id="email"
+                                        name="email"
+                                        placeholder="student@ohsea.gg"
+                                        as="input"
+                                        validate={validateEmail}
+                                        />
+                                        {errors.email && touched.email && 
+                                        <div className="error"><RiErrorWarningLine className="warning-icon"/> {errors.email}</div>
+                                        }
+                                    </div>
+                                    <div className="input-field">
+                                        <label htmlFor="msg">How can we help?</label>
+                                        <Field
+                                        id="msg"
+                                        name="msg"
+                                        placeholder="Your Message"
+                                        as="textarea"
+                                        validate={validateMessage}
+                                        />
+                                        {errors.msg && touched.msg && 
+                                        <div className="error"><RiErrorWarningLine className="warning-icon"/> {errors.msg}</div>
+                                        }
+                                    </div>
+                                    <button 
+                                    type="submit"
+                                    className="btn-fill"
+                                    >Send</button>
+                                </div>
+                            </Form>
+                        )}
+                        </Formik>
+                    </div>
                 </div>
-            </Layout>
-        )
-    }
+            </div>
+        </Layout>
+    )
 }
-
-export default Contact

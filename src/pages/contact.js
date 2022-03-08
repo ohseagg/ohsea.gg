d1844b1e1367e544d42347221f799322e0735031
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2022-03-07 21:30:17 -0500

    feat: basic contact form elements
    also add emailjs and formik to package

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import  { useRef } from "react"
+// import emailjs from '@emailjs/browser';
+import { Formik, Field, Form  } from 'formik'
 
 import Layout from "../components/PageLayout/layout"
 import Seo from "../components/SEO/seo"
@@ -10,7 +13,42 @@ class Contact extends React.Component {
                 <Seo title="Contact" />
                 <div className="plain-dark-bg">
                     <div className="container-contact">
-                        
+                        <div className="left">
+                            
+                        </div>
+                        <div className="right">
+                        <Formik
+                            initialValues={{
+                                firstName: '',
+                                lastName: '',
+                                email: '',
+                            }}
+                            onSubmit={async (values) => {
+                                await new Promise((r) => setTimeout(r, 500));
+                                alert(JSON.stringify(values, null, 2));
+                            }}
+                            >
+                            <Form>
+                                <div className="form-wrapper">
+                                    <div className="input-field">
+                                        <label htmlFor="email">Your email address</label>
+                                        <Field
+                                            id="email"
+                                            name="email"
+                                            placeholder="student@ohsea.gg"
+                                            type="email"
+                                            as="input"
+                                        />
+                                    </div>
+                                    <div className="input-field">
+                                        <label htmlFor="subject">How can we help?</label>
+                                        <Field id="subject" name="subject" placeholder="Your Message" as="textarea"/>
+                                    </div>
+                                    <button type="submit" className="btn-fill">Submit</button>
+                                </div>
+                            </Form>
+                            </Formik>
+                        </div>
                     </div>
                 </div>
             </Layout>
